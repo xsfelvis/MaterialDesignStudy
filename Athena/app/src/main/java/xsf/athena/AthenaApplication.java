da1e05ae3800ea16ef6b65b35a33bd4c9f0b857e
uestc_xsf <xsf_uestc_ncl@163.com>
2016-04-22 10:30:41 +0800

    add custom application

---
+++ Athena/app/src/main/java/xsf/athena/AthenaApplication.java
@@ -0,0 +1,31 @@
+package xsf.athena;
+
+import android.app.Application;
+
+import com.orhanobut.logger.Logger;
+
+/**
+ * Author: xsf
+ * Time: created at 2016/4/22.
+ * Email: xsf_uestc_ncl@163.com
+ */
+public class AthenaApplication extends Application {
+    public static AthenaApplication AnthenaApp;
+
+    public static AthenaApplication get() {
+        return AnthenaApp;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AnthenaApp = this;
+        //初始化logger系统
+        Logger
+                .init("Athena")                 // default PRETTYLOGGER or use just init()
+                .methodCount(2)                 // default 2
+                .hideThreadInfo()               // default shown
+                .methodOffset(2);                // default 0
+
+    }
+}
