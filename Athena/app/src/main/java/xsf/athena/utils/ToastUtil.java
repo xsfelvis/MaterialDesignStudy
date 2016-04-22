1ae232d50db23c7c1e4541c7d4cca8edf9e90ee9
uestc_xsf <xsf_uestc_ncl@163.com>
2016-04-22 11:14:30 +0800

    add custom toast util

---
+++ Athena/app/src/main/java/xsf/athena/utils/ToastUtil.java
@@ -0,0 +1,52 @@
+package xsf.athena.utils;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import xsf.athena.AthenaApplication;
+import xsf.athena.R;
+
+/**
+ * Author: xsf
+ * Time: created at 2016/4/22.
+ * Email: xsf_uestc_ncl@163.com
+ */
+public class ToastUtil {
+    public static void show(String text, int showtime) {
+        LayoutInflater inflater = (LayoutInflater) AthenaApplication.get().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.toast_layout, null);
+        TextView content = (TextView) layout.findViewById(R.id.tv_content);
+        if (!Tools.isEmpty(text)) {
+            content.setText(text);
+        } else {
+            content.setText(R.string.err_system);
+        }
+        Toast toast = new Toast(AthenaApplication.get());
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setDuration(showtime);
+        toast.setView(layout);
+        toast.show();
+    }
+
+    public static void showWithImg(String text, int showtime) {
+        LayoutInflater inflater = (LayoutInflater) AthenaApplication.get().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.toast_layout_with_img, null);
+        TextView content = (TextView) layout.findViewById(R.id.tv_content);
+        if (!Tools.isEmpty(text)) {
+            content.setText(text);
+        } else {
+            content.setText(R.string.err_system);
+        }
+        Toast toast = new Toast(AthenaApplication.get());
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setDuration(showtime);
+        toast.setView(layout);
+        toast.show();
+    }
+
+
+}
