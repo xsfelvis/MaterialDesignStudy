29b70f08d8371076ec4c1c9e269fd28c4d81b9fe
uestc_xsf <xsf_uestc_ncl@163.com>
2016-04-25 14:49:31 +0800

    add fileutil

--- Athena/app/src/main/java/xsf/athena/utils/Tools.java
+++ Athena/app/src/main/java/xsf/athena/utils/Tools.java
@@ -132,4 +132,30 @@ public class Tools {
     public boolean isNullOrEmpty(String s) {
         return s == null || "".equals(s.trim());
     }
+
+    /**
+     * 判断网络是否正常连接
+     *
+     * @param context
+     * @return boolean true：网络连接正常 false：网络连接不正常
+     */
+    public static boolean isNetworkConnected(Context context) {
+        if (null == context) {
+            return false;
+        }
+        // 获取手机所有连接管理对象（包括wifi，net等连接的管理）
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            // 获取网络连接管理的对象
+            NetworkInfo info = connectivityManager.getActiveNetworkInfo();
+            if (info != null && info.isConnected()) {
+                // 判断当前网络是否已经连接
+                if (info.getState() == NetworkInfo.State.CONNECTED) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
